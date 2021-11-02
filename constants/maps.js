17ac6ba450a1141eea13cd45a863338fb87789d7
Richard <richardchadori@gmail.com>
2021-11-02 02:44:33 -0700

    Added in from local file

---
+++ constants/maps.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY = "AIzaSyBr_dDFMVT7LlqVaDofrrqYruudu4XmWRU"
+//AIzaSyBr_dDFMVT7LlqVaDofrrqYruudu4XmWRU
+
+export default GOOGLE_API_KEY
---
+++ screens/OrderDelivery.js
@@ -0,0 +1,15 @@
+import REACT from "react";
+import {
+    View,
+    Text
+} from "react-native";
+
+const OrderDelivery = () => {
+    return (
+        <View>
+            <Text>OrderDelivery</Text>
+        </View>
+    )
+}
+
+export default Restaurant;
